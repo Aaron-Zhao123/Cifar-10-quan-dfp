f4feddc61f78ca3f92bff5749119b21be6f18d21
Aaron-Zhao123 <aaronzhao0731@gmail.com>
2017-04-21 16:29:22 +0100

    added cifar file

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ retrain = 0
 parent_dir = './'
 base_model = 'base.pkl'
 quantisation_bits = [2, 4, 8, 16, 32, 64]
-quantisation_bits = [32, 64]
+# quantisation_bits = [32, 64]
 pcov = [0,0]
 dynamic_range = 4
 for q_width in quantisation_bits:
